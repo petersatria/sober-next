58071ec6ea54c35e24416dceec5c96195c785dce
skuukzky14 <idhamdummy1@gmail.com>
2022-10-14 19:02:55 +0700

    feature:integrate newsletter features with server

--- components/Home/Newsletter/Newsletter.js
+++ components/Home/Newsletter/Newsletter.js
@@ -1,14 +1,21 @@
 import { useRef } from 'react';
-
+import axios from 'axios'
 import styles from './Newsletter.module.css';
 
 const Newsletter = () => {
     const inputRef = useRef();
 
-    const submitHandler = (e) => {
+    const submitHandler = async(e) => {
         e.preventDefault();
+        const email = inputRef.current.value
 
-        console.log(inputRef.current.value);
+        try {
+            const emailResponse = await axios.post(`http://localhost:5000/newmail`,{email})
+            console.log(emailResponse)
+        } catch (error) {
+            console.log(error)
+        }
+
 
         inputRef.current.value = '';
     };
